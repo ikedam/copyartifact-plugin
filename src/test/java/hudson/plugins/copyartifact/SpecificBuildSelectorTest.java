62458ec7b086b2b165999821f3885dcc3316c883
ikedam <devld@ikedam.jp>
2015-10-17 14:45:05 +0900

    [JENKINS-24892] Migrated PermalinkBuildSelector, SpecificBuildSelector to the new mechanism.

--- src/test/java/hudson/plugins/copyartifact/SpecificBuildSelectorTest.java
+++ src/test/java/hudson/plugins/copyartifact/SpecificBuildSelectorTest.java
@@ -1,9 +1,16 @@
 package hudson.plugins.copyartifact;
 
 import hudson.EnvVars;
+import hudson.model.Cause;
 import hudson.model.FreeStyleProject;
+import hudson.model.ParametersAction;
+import hudson.model.Run;
+import hudson.model.StringParameterValue;
+import hudson.plugins.copyartifact.testutils.CopyArtifactUtil;
+
 import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.CaptureEnvironmentBuilder;
 
 public class SpecificBuildSelectorTest extends HudsonTestCase {
 
@@ -14,10 +21,39 @@ public class SpecificBuildSelectorTest extends HudsonTestCase {
         assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertEquals(3, p.getLastBuild().number);
-        BuildSelector s = new SpecificBuildSelector("$NUM");
-        BuildFilter f = new BuildFilter();
-        assertEquals(p.getBuildByNumber(2), s.getBuild(p, new EnvVars("NUM", "2"), f, null));
-        assertEquals(null, s.getBuild(p, new EnvVars("HUM", "two"), f, null));
+        
+        FreeStyleProject copier = createFreeStyleProject();
+        copier.getBuildersList().add(CopyArtifactUtil.createCopyArtifact(
+                p.getFullDisplayName(),
+                "",     // parameter
+                new SpecificBuildSelector("$NUM"),
+                "**/*", // filter
+                "",     // excludes
+                "",     // target
+                false,  // flatten
+                true,   // optional
+                false,  // fingerprintArtifacts
+                "RESULT"// resultVariableSuffix
+        ));
+        CaptureEnvironmentBuilder ceb = new CaptureEnvironmentBuilder();
+        copier.getBuildersList().add(ceb);
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "2")
+                )
+        ));
+        assertEquals("2", ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("HUM", "two")
+                )
+        ));
+        assertEquals(null, ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
     }
 
     @Bug(19693)
@@ -28,10 +64,46 @@ public class SpecificBuildSelectorTest extends HudsonTestCase {
         assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertEquals(3, p.getLastBuild().number);
         p.getBuildByNumber(2).setDisplayName("RC1");
-        BuildSelector s = new SpecificBuildSelector("$NUM");
-        BuildFilter f = new BuildFilter();
-        assertEquals(p.getBuildByNumber(2), s.getBuild(p, new EnvVars("NUM", "RC1"), f, null));
-        assertEquals(null, s.getBuild(p, new EnvVars("NUM", "RC2"), f, null));
+        
+        FreeStyleProject copier = createFreeStyleProject();
+        copier.getBuildersList().add(CopyArtifactUtil.createCopyArtifact(
+                p.getFullDisplayName(),
+                "",     // parameter
+                new SpecificBuildSelector("$NUM"),
+                "**/*", // filter
+                "",     // excludes
+                "",     // target
+                false,  // flatten
+                true,   // optional
+                false,  // fingerprintArtifacts
+                "RESULT"// resultVariableSuffix
+        ));
+        CaptureEnvironmentBuilder ceb = new CaptureEnvironmentBuilder();
+        copier.getBuildersList().add(ceb);
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "RC1")
+                )
+        ));
+        assertEquals("2", ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "RC2")
+                )
+        ));
+        assertEquals(null, ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+    }
+
+    private String getBuildNumberOf(Run<?,?> r) {
+        if (r == null) {
+            return null;
+        }
+        return Integer.toString(r.getNumber());
     }
 
     public void testPermalink() throws Exception {
@@ -40,14 +112,76 @@ public class SpecificBuildSelectorTest extends HudsonTestCase {
         assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertEquals(3, p.getLastBuild().number);
-        BuildSelector s = new SpecificBuildSelector("$NUM");
-        BuildFilter f = new BuildFilter();
-        assertEquals(p.getLastSuccessfulBuild(), s.getBuild(p, new EnvVars("NUM", "lastSuccessfulBuild"), f, null));
-        assertEquals(p.getLastStableBuild(), s.getBuild(p, new EnvVars("NUM", "lastStableBuild"), f, null));
-        assertEquals(p.getLastBuild(), s.getBuild(p, new EnvVars("NUM", "lastBuild"), f, null));
-        assertEquals(p.getLastFailedBuild(), s.getBuild(p, new EnvVars("NUM", "lastFailedBuild"), f, null));
-        assertEquals(p.getLastUnstableBuild(), s.getBuild(p, new EnvVars("NUM", "lastUnstableBuild"), f, null));
-        assertEquals(p.getLastUnsuccessfulBuild(), s.getBuild(p, new EnvVars("NUM", "lastUnsuccessfulBuild"), f, null));
+        
+        FreeStyleProject copier = createFreeStyleProject();
+        copier.getBuildersList().add(CopyArtifactUtil.createCopyArtifact(
+                p.getFullDisplayName(),
+                "",     // parameter
+                new SpecificBuildSelector("$NUM"),
+                "**/*", // filter
+                "",     // excludes
+                "",     // target
+                false,  // flatten
+                true,   // optional
+                false,  // fingerprintArtifacts
+                "RESULT"// resultVariableSuffix
+        ));
+        CaptureEnvironmentBuilder ceb = new CaptureEnvironmentBuilder();
+        copier.getBuildersList().add(ceb);
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "lastSuccessfulBuild")
+                )
+        ));
+        assertEquals(getBuildNumberOf(p.getLastSuccessfulBuild()), ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "lastStableBuild")
+                )
+        ));
+        assertEquals(getBuildNumberOf(p.getLastStableBuild()), ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "lastBuild")
+                )
+        ));
+        assertEquals(getBuildNumberOf(p.getLastBuild()), ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "lastFailedBuild")
+                )
+        ));
+        assertEquals(getBuildNumberOf(p.getLastFailedBuild()), ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "lastUnstableBuild")
+                )
+        ));
+        assertEquals(getBuildNumberOf(p.getLastUnstableBuild()), ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
+        
+        assertBuildStatusSuccess(copier.scheduleBuild2(
+                0,
+                new Cause.UserIdCause(),
+                new ParametersAction(
+                        new StringParameterValue("NUM", "lastUnsuccessfulBuild")
+                )
+        ));
+        assertEquals(getBuildNumberOf(p.getLastUnsuccessfulBuild()), ceb.getEnvVars().get("COPYARTIFACT_BUILD_NUMBER_RESULT"));
     }
 
 }
